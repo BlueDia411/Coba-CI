f71550095231c2bb435ac8f32e68e3178c785c9a
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-23 20:05:45 +0700

    menambah peoples pada view

---
+++ application/views/peoples/index.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+        <h3 class="mt-3">List Of Peoples</h3>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th>Action</th>
+                </tr>    
+            </thead>
+                <tbody>
+                    <tr>
+                        <th>1</th>
+                        <td>Yogi Nugraha</td>
+                        <td>yoginugraha43@gmail.com</td>
+                        <td>
+                        <a href="" class="badge badge-Warning">Detail</a>
+                        <a href="" class="badge badge-success">Edit</a>
+                        <a href="" class="badge badge-danger">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+        </table>
+        
+        </div>
+    </div>
+</div>
