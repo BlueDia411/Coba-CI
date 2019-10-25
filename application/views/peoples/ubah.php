68b8b4d03a433868cf7538b0f5e616bbc29f631c
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-25 08:50:36 +0700

    fungsi action pada peoples belum berjalan

--- application/views/peoples/ubah.php
+++ application/views/peoples/ubah.php
@@ -28,26 +28,15 @@
                         <small class="form-text text-danger"><?= form_error('nama'); ?></small>
                     </div>
                     <div class="form-group">
-                        <label for="nrp">NRP</label>
-                        <input type="text" class="form-control" id="nrp" name="nrp" value="<?= $mahasiswa['nrp']; ?>">
-                        <small class="form-text text-danger"><?= form_error('nrp'); ?></small>
+                        <label for="address">Address</label>
+                        <input type="text" class="form-control" id="address" name="address" value="<?= $mahasiswa['address']; ?>">
+                        <small class="form-text text-danger"><?= form_error('address'); ?></small>
                     </div>
                     <div class="form-group">
                         <label for="email">E-Mail</label>
                         <input type="text" class="form-control" id="email" name="email" value="<?= $mahasiswa['email']; ?>">
                         <small class="form-text text-danger"><?= form_error('email'); ?></small>
                     </div>
-                    <div class="form-group">
-                        <label for="jurusan">Jurusan</label>
-                        <select class="form-control" id="jurusan" name="jurusan">
-                        <?php foreach($jurusan as $j) : ?>
-                            <?php if( $j == $mahasiswa['jurusan'] ) : ?>
-                                <option value="<?= $j?>" selected><?= $j?></option>
-                            <?php else : ?>
-                                <option value="<?= $j?>"><?= $j?></option>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                        </select>
                     </div>
                     <button type="submit" class="btn btn-primary float-right" name="ubah">Ubah</button>
                     </form>
